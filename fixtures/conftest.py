43fdb0caeef4e6249433cc887a0ad93ac43c76f1
Maria Vafolomeeva <ewds.war@yandex.ru>
2025-02-22 19:56:43 +0300

    fixed conftest.py nd test_constructor.py

--- fixtures/conftest.py
+++ fixtures/conftest.py
@@ -2,10 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.firefox import GeckoDriverM
+
+BASE_URL = "https://stellarburgers.site"
+BURGER_URL = f"{BASE_URL}/burger"
+ACCOUNT_URL = f"{BASE_URL}/account"
+REGISTER_URL = f"{BASE_URL}/register"
+RECOVERY_URL = f"{BASE_URL}/recovery"
+LOGIN_URL = f"{BASE_URL}/login"
 
 @pytest.fixture
-def driver(browser='chrome'):
+def get_driver(browser='chrome'):
     """ Функция для получения WebDriver для выбранного браузера """
     if browser == 'chrome':
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,7 +1,6 @@
 import pytest
 from utils.locators import *
-from fixtures.conftest import get_driver
-
+from fixtures.conftest import BURGER_URL
 
 @pytest.fixture
 def driver():
@@ -11,21 +10,16 @@ def driver():
 
 
 def test_transition_to_buns_section(driver):
-    driver.get("https://stellarburgers.site/burger")
+    driver.get(BURGER_URL)
     driver.find_element(*BUNS_SECTION).click()
     assert "buns" in driver.current_url
-    driver.quit()
-
 
 def test_transition_to_sauces_section(driver):
-    driver.get("https://stellarburgers.site/burger")
+    driver.get(BURGER_URL)
     driver.find_element(*SAUCES_SECTION).click()
     assert "sauces" in driver.current_url
-    driver.quit()
-
 
 def test_transition_to_fillings_section(driver):
-    driver.get("https://stellarburgers.site/burger")
+    driver.get(BURGER_URL)
     driver.find_element(*FILLINGS_SECTION).click()
     assert "fillings" in driver.current_url
-    driver.quit()
